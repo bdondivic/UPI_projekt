9fc6a4c0ace2005862add194a3357ca7e2dae25b
Marin Novaković <mnovakovi@pmfst.hr>
2022-12-12 11:58:05 +0100

    Izgled pretrage

--- MainUser.Designer.cs
+++ MainUser.Designer.cs
@@ -28,33 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpPretraga = new System.Windows.Forms.TabPage();
+            this.tpBacklog = new System.Windows.Forms.TabPage();
+            this.tpIgram = new System.Windows.Forms.TabPage();
+            this.tpIgrao = new System.Windows.Forms.TabPage();
+            this.tpProfil = new System.Windows.Forms.TabPage();
+            this.txtPretraga = new System.Windows.Forms.TextBox();
+            this.lbIgre = new System.Windows.Forms.ListBox();
+            this.rtbOpis = new System.Windows.Forms.RichTextBox();
+            this.cbZanr = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tpPretraga.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // tabControl1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 135);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "KORISNIK!!!";
+            this.tabControl1.Controls.Add(this.tpPretraga);
+            this.tabControl1.Controls.Add(this.tpBacklog);
+            this.tabControl1.Controls.Add(this.tpIgram);
+            this.tabControl1.Controls.Add(this.tpIgrao);
+            this.tabControl1.Controls.Add(this.tpProfil);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1006, 721);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tpPretraga
+            // 
+            this.tpPretraga.Controls.Add(this.cbZanr);
+            this.tpPretraga.Controls.Add(this.rtbOpis);
+            this.tpPretraga.Controls.Add(this.lbIgre);
+            this.tpPretraga.Controls.Add(this.txtPretraga);
+            this.tpPretraga.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tpPretraga.Location = new System.Drawing.Point(4, 25);
+            this.tpPretraga.Name = "tpPretraga";
+            this.tpPretraga.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPretraga.Size = new System.Drawing.Size(998, 692);
+            this.tpPretraga.TabIndex = 0;
+            this.tpPretraga.Text = "PRETRAGA";
+            this.tpPretraga.UseVisualStyleBackColor = true;
+            // 
+            // tpBacklog
+            // 
+            this.tpBacklog.Location = new System.Drawing.Point(4, 25);
+            this.tpBacklog.Name = "tpBacklog";
+            this.tpBacklog.Padding = new System.Windows.Forms.Padding(3);
+            this.tpBacklog.Size = new System.Drawing.Size(998, 692);
+            this.tpBacklog.TabIndex = 1;
+            this.tpBacklog.Text = "BACKLOG";
+            this.tpBacklog.UseVisualStyleBackColor = true;
+            // 
+            // tpIgram
+            // 
+            this.tpIgram.Location = new System.Drawing.Point(4, 25);
+            this.tpIgram.Name = "tpIgram";
+            this.tpIgram.Padding = new System.Windows.Forms.Padding(3);
+            this.tpIgram.Size = new System.Drawing.Size(998, 692);
+            this.tpIgram.TabIndex = 2;
+            this.tpIgram.Text = "IGRAM";
+            this.tpIgram.UseVisualStyleBackColor = true;
+            // 
+            // tpIgrao
+            // 
+            this.tpIgrao.Location = new System.Drawing.Point(4, 25);
+            this.tpIgrao.Name = "tpIgrao";
+            this.tpIgrao.Size = new System.Drawing.Size(998, 692);
+            this.tpIgrao.TabIndex = 3;
+            this.tpIgrao.Text = "IGRAO";
+            this.tpIgrao.UseVisualStyleBackColor = true;
+            // 
+            // tpProfil
+            // 
+            this.tpProfil.Location = new System.Drawing.Point(4, 25);
+            this.tpProfil.Name = "tpProfil";
+            this.tpProfil.Size = new System.Drawing.Size(998, 692);
+            this.tpProfil.TabIndex = 4;
+            this.tpProfil.Text = "PROFIL";
+            this.tpProfil.UseVisualStyleBackColor = true;
+            // 
+            // txtPretraga
+            // 
+            this.txtPretraga.Location = new System.Drawing.Point(33, 34);
+            this.txtPretraga.Name = "txtPretraga";
+            this.txtPretraga.Size = new System.Drawing.Size(434, 22);
+            this.txtPretraga.TabIndex = 0;
+            this.txtPretraga.TextChanged += new System.EventHandler(this.txtPretraga_TextChanged);
+            // 
+            // lbIgre
+            // 
+            this.lbIgre.FormattingEnabled = true;
+            this.lbIgre.ItemHeight = 16;
+            this.lbIgre.Location = new System.Drawing.Point(33, 94);
+            this.lbIgre.Name = "lbIgre";
+            this.lbIgre.Size = new System.Drawing.Size(434, 468);
+            this.lbIgre.TabIndex = 1;
+            // 
+            // rtbOpis
+            // 
+            this.rtbOpis.Location = new System.Drawing.Point(488, 94);
+            this.rtbOpis.Name = "rtbOpis";
+            this.rtbOpis.Size = new System.Drawing.Size(492, 195);
+            this.rtbOpis.TabIndex = 2;
+            this.rtbOpis.Text = "";
+            // 
+            // cbZanr
+            // 
+            this.cbZanr.FormattingEnabled = true;
+            this.cbZanr.Location = new System.Drawing.Point(503, 32);
+            this.cbZanr.Name = "cbZanr";
+            this.cbZanr.Size = new System.Drawing.Size(196, 24);
+            this.cbZanr.TabIndex = 3;
             // 
             // MainUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainUser";
+            this.Load += new System.EventHandler(this.MainUser_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tpPretraga.ResumeLayout(false);
+            this.tpPretraga.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tpPretraga;
+        private System.Windows.Forms.TabPage tpBacklog;
+        private System.Windows.Forms.TabPage tpIgram;
+        private System.Windows.Forms.TabPage tpIgrao;
+        private System.Windows.Forms.TabPage tpProfil;
+        private System.Windows.Forms.ComboBox cbZanr;
+        private System.Windows.Forms.RichTextBox rtbOpis;
+        private System.Windows.Forms.ListBox lbIgre;
+        private System.Windows.Forms.TextBox txtPretraga;
     }
 }
--- MainUser.cs
+++ MainUser.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Backlog
 {
@@ -13,6 +16,23 @@ namespace Backlog
         public MainUser()
         {
             InitializeComponent();
+        }
+
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OlEDB.4.0;Data Source=db_Backlog.mdb");
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da = new OleDbDataAdapter();
+        
+
+        
+
+        private void txtPretraga_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainUser_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
