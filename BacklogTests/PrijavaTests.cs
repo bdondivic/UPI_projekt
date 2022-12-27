369a7d368f3d5b42e63a4fc72f8720137c539b39
Marin Novaković <mnovakovi@pmfst.hr>
2022-12-27 14:37:10 +0100

    Dodana mogućnost uklanjanja igara iz listi

---
+++ BacklogTests/PrijavaTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Tests
+{
+    [TestClass()]
+    public class PrijavaTests
+    {
+        [TestMethod()]
+        public void KrivaLozinkaTest()
+        {
+            string user = "test123";
+            string pass = "kriva_lozinka";
+
+            Prijava p = new Prijava(user, pass);
+
+            string expected = "Neispravna lozinka!";
+            string actual = p.Provjera();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void KriviUsernameTest()
+        {
+            string user = "test1234";
+            string pass = "test123";
+
+            Prijava p = new Prijava(user, pass);
+
+            string expected = "Ne postoji korisnik s tim korisničkim imenom!";
+            string actual = p.Provjera();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PraznaPrijavaTest()
+        {
+            string user = "";
+            string pass = "";
+
+            Prijava p = new Prijava(user, pass);
+
+            string expected = "Ne postoji korisnik s tim korisničkim imenom!";
+            string actual = p.Provjera();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void UspjesnaPrijavaTest()
+        {
+            string user = "test123";
+            string pass = "test123";
+            
+            Prijava p = new Prijava(user, pass);
+
+            string expected = "Prijava OK!";
+            string actual = p.Provjera();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
